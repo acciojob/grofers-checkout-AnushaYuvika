fb656aa8933e6df755e61a7cee5962ca361c27c1
Anusha Yuvika <151361373+AnushaYuvika@users.noreply.github.com>
2026-03-05 21:04:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,10 +2,22 @@ const getSumBtn = document.createElement("button");
 getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
+const result = document.createElement("h3");
+document.body.appendChild(result);
+
 const getSum = () => {
-//Add your code here
-  
+  let totalPrice = 0;
+  const prices = document.querySelectorAll(".price");
+
+  prices.forEach((price) => {
+    totalPrice += Number(price.textContent);
+  });
+
+  return totalPrice;
 };
 
-getSumBtn.addEventListener("click", getSum);
-
+// Button Click Event
+getSumBtn.addEventListener("click", () => {
+  const total = getSum();
+  result.textContent = `Total Price: ₹${total}`;
+});
