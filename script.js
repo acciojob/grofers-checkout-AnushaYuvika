3096f10fede2df9e96117b6554891c4beea7bae8
Anusha Yuvika <151361373+AnushaYuvika@users.noreply.github.com>
2026-03-09 13:24:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,56 @@
+// const getSumBtn = document.createElement("button");
+// getSumBtn.append("Get Total Price");
+// document.body.appendChild(getSumBtn);
+
+// const prices = document.querySelectorAll(".price");
+
+// getSumBtn.addEventListener("click", () => {
+
+// 	let total = 0;
+
+// 	prices.forEach((price) => {
+// 		const value = Number(price.textContent);
+// 		total += value;
+// 	});
+
+// 	const row = document.createElement("tr");
+
+// 	const totalLabel = document.createElement("td");
+// 	totalLabel.textContent = "Total";
+
+// 	const totalPrice = document.createElement("td");
+// 	totalPrice.textContent = total;
+
+// 	row.appendChild(totalLabel);
+// 	row.appendChild(totalPrice);
+
+// 	document.querySelector("table").appendChild(row);
+// 	getSumBtn.disabled = true;
+// });
+
 const getSumBtn = document.createElement("button");
-getSumBtn.append("Get Total Price");
-document.body.appendChild(getSumBtn);
+getSumBtn.textContent = "Get Total Price";
+document.querySelector("table").appendChild(getSumBtn);
 
-const result = document.createElement("h3");
-document.body.appendChild(result);
+const prices = document.querySelectorAll(".price");
 
-const getSum = () => {
-  let totalPrice = 0;
-  const prices = document.querySelectorAll(".price");
-
-  prices.forEach((price) => {
-    totalPrice += Number(price.textContent);
-  });
-
-  return totalPrice;
-};
-
-// Button Click Event
 getSumBtn.addEventListener("click", () => {
-  const total = getSum();
-  result.textContent = `Total Price: ₹${total}`;
-});
+	let total = 0;
+	
+	prices.forEach((price) => {
+		const value = Number(price.textContent);
+		total += value;
+	})
+
+	const row = document.createElement("tr");
+	const totalLabel = document.createElement("td");
+	totalLabel.textContent = "Total";
+	const totalPrice = document.createElement("td");
+	totalPrice.textContent = total;
+
+	row.appendChild(totalLabel);
+	row.appendChild(totalPrice);
+
+	document.querySelector("table").appendChild(row);
+	getSumBtn.disabled = true;
+})
