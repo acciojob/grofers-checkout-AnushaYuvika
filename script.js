a31935d239335a881c01034ed296a689383214bf
Anusha Yuvika <151361373+AnushaYuvika@users.noreply.github.com>
2026-04-02 17:28:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,20 @@
 // const getSumBtn = document.createElement("button");
-// getSumBtn.append("Get Total Price");
-// document.body.appendChild(getSumBtn);
+// getSumBtn.textContent = "Get Total Price";
+// document.querySelector("table").appendChild(getSumBtn);
 
 // const prices = document.querySelectorAll(".price");
 
 // getSumBtn.addEventListener("click", () => {
-
 // 	let total = 0;
-
+	
 // 	prices.forEach((price) => {
 // 		const value = Number(price.textContent);
 // 		total += value;
-// 	});
+// 	})
 
 // 	const row = document.createElement("tr");
-
 // 	const totalLabel = document.createElement("td");
 // 	totalLabel.textContent = "Total";
-
 // 	const totalPrice = document.createElement("td");
 // 	totalPrice.textContent = total;
 
@@ -26,7 +23,7 @@
 
 // 	document.querySelector("table").appendChild(row);
 // 	getSumBtn.disabled = true;
-// });
+// })
 
 const getSumBtn = document.createElement("button");
 getSumBtn.textContent = "Get Total Price";
@@ -38,9 +35,8 @@ getSumBtn.addEventListener("click", () => {
 	let total = 0;
 	
 	prices.forEach((price) => {
-		const value = Number(price.textContent);
-		total += value;
-	})
+		total += Number(price.textContent);
+	});
 
 	const row = document.createElement("tr");
 	const totalLabel = document.createElement("td");
@@ -52,5 +48,5 @@ getSumBtn.addEventListener("click", () => {
 	row.appendChild(totalPrice);
 
 	document.querySelector("table").appendChild(row);
-	getSumBtn.disabled = true;
+	getSumBtn.style.visibility = "hidden";
 })
